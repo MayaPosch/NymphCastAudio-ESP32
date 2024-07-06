33650b00519a77894d09cebbe949fb57312ba200
Maya Posch <maya@nyanko.ws>
2024-07-06 14:07:06 +0200

    [BROKEN] Enable HTTP(S), TLS support. Breaks on TCP redef.

--- components/libavformat/libavformat/protocol_list.c
+++ components/libavformat/libavformat/protocol_list.c
@@ -1,4 +1,8 @@
 static const URLProtocol * const url_protocols[] = {
     &ff_file_protocol,
     &ff_pipe_protocol,
+	&ff_http_protocol,
+	&ff_https_protocol,
+	&ff_tls_protocol,
+	&ff_tcp_protocol,
     NULL };
